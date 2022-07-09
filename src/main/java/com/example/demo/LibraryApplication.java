f7fb93864fab82310d3ecd26acecb5a02950d4cd
Fhellipe Valentin <fhellipereis@outlook.com>
2022-07-09 12:43:54 -0300

    Criando operacao de instanciacao

--- src/main/java/com/example/demo/LibraryApplication.java
+++ src/main/java/com/example/demo/LibraryApplication.java
@@ -1,13 +1,32 @@
 package com.example.demo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class LibraryApplication {
+import com.example.demo.entities.Categoria;
+import com.example.demo.entities.repository.CategoriaRepository;
 
+@SpringBootApplication
+public class LibraryApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
 	}
-
+	
+	@Override
+	public void run (String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Comércio");
+		Categoria cat2 = new Categoria(null, "Jurídicos");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
 }
