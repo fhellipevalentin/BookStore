0e1bf53f9a2b7664a75eccb5bfc8a89fb3586656
Fhellipe Valentin <fhellipereis@outlook.com>
2022-11-03 17:01:58 -0300

    findAll com DTO de Categorias

--- src/main/java/io/github/fhellipe/bookstore/controller/CategoriaController.java
+++ src/main/java/io/github/fhellipe/bookstore/controller/CategoriaController.java
@@ -1,5 +1,6 @@
 package io.github.fhellipe.bookstore.controller;
 
+import io.github.fhellipe.bookstore.dto.CategoriaDTO;
 import io.github.fhellipe.bookstore.model.Categoria;
 import io.github.fhellipe.bookstore.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -42,5 +45,12 @@ public class CategoriaController {
     public ResponseEntity<Void> delete(@PathVariable Integer id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoriaDTO>> findAll() {
+        List<Categoria> list = service.findAll();
+        List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
