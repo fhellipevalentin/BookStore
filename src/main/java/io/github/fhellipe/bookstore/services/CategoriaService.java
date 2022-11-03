e5e891a7eb272cea3f804540e02c1ff76bb7c2fc
Fhellipe Valentin <fhellipereis@outlook.com>
2022-11-03 17:24:08 -0300

    Bean Validation e adequacao DTO para controlador

--- src/main/java/io/github/fhellipe/bookstore/services/CategoriaService.java
+++ src/main/java/io/github/fhellipe/bookstore/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package io.github.fhellipe.bookstore.services;
 
+import io.github.fhellipe.bookstore.dto.CategoriaDTO;
 import io.github.fhellipe.bookstore.model.Categoria;
 import io.github.fhellipe.bookstore.repositories.CategoriaRepository;
 import io.github.fhellipe.bookstore.services.exceptions.DataIntegrityException;
@@ -52,5 +53,9 @@ public class CategoriaService {
     public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
         PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
         return repository.findAll(pageRequest);
+    }
+
+    public Categoria fromDTO(CategoriaDTO objDto) {
+        return new Categoria(objDto.getId(), objDto.getNome());
     }
 }
