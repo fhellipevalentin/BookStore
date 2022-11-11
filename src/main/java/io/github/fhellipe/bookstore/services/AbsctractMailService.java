6d79ac6988b033efd084816241f80da604b23700
Fhellipe Valentin <fhellipereis@outlook.com>
2022-11-11 17:19:01 -0300

    Email HTML

--- src/main/java/io/github/fhellipe/bookstore/services/AbsctractMailService.java
+++ src/main/java/io/github/fhellipe/bookstore/services/AbsctractMailService.java
@@ -1,15 +1,28 @@
 package io.github.fhellipe.bookstore.services;
 
 import io.github.fhellipe.bookstore.model.Pedido;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.util.Date;
 
 public abstract class AbsctractMailService implements EmailService{
 
+    @Autowired
+    private TemplateEngine templateEngine;
+
     @Value("${default-sender}")
     private String sender;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
 
     @Override
     public void sendOrderConfirmationEmail(Pedido obj) {
@@ -25,5 +38,33 @@ public abstract class AbsctractMailService implements EmailService{
         sm.setSentDate(new Date(System.currentTimeMillis()));
         sm.setText(obj.toString());
         return sm;
+    }
+
+    protected String htmlFromTemplatePedido(Pedido obj) {
+        Context context = new Context();
+        context.setVariable("pedido", obj);
+        return templateEngine.process("email/confirmacaoPedido", context);
+    }
+
+    @Override
+    public void sendOrderConfirmationHtmlEmail(Pedido obj) {
+        try {
+            MimeMessage mm = prepareMimeMessageFromPedido(obj);
+            sendHtmlEmail(mm);
+        }
+        catch (MessagingException e) {
+            sendOrderConfirmationEmail(obj);
+        }
+    }
+
+    protected MimeMessage prepareMimeMessageFromPedido(Pedido obj) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mmh = new MimeMessageHelper(mimeMessage, true);
+        mmh.setTo(obj.getUsuario().getEmail());
+        mmh.setFrom(sender);
+        mmh.setSubject("Pedido Confirmado, Código" + obj.getId());
+        mmh.setSentDate(new Date(System.currentTimeMillis()));
+        mmh.setText(htmlFromTemplatePedido(obj), true);
+        return mimeMessage;
     }
 }
--- src/main/java/io/github/fhellipe/bookstore/services/EmailService.java
+++ src/main/java/io/github/fhellipe/bookstore/services/EmailService.java
@@ -3,8 +3,14 @@ package io.github.fhellipe.bookstore.services;
 import io.github.fhellipe.bookstore.model.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 public interface EmailService {
 
     void sendOrderConfirmationEmail(Pedido obj);
     void sendEmail(SimpleMailMessage msg);
+
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+    void sendHtmlEmail(MimeMessage msg);
 }
